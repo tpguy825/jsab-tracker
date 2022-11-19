fbebd8a07db3b2bdf34c7ce8ae16db20c84d4717
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-11-19 09:22:09 +0000

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -225,32 +225,3 @@ interface Utils {
 	 * ``` */
 	getLocalStorage(key: string): string | null;
 }
-
-interface MainConfig {
-	firebase: FirebaseConfig;
-	analytics: AnalyticsConfig;
-	footermessage?: string | JSX.Element;
-}
-
-type AnalyticsConfig = {
-	/** Whether to use Google Analytics */
-	enabled: boolean;
-
-	/** Google Analytics tracking ID. `undefined` if not enabled */
-	id?: string;
-
-	/** Whether to use Partytown */
-	partytown: boolean;
-}
-
-interface FirebaseConfig {
-	apiKey: string;
-	authDomain: string;
-	projectId: string;
-	storageBucket: string;
-	messagingSenderId: string;
-	appId: string;
-	measurementId: string;
-	databaseURL: string;
-}
-
