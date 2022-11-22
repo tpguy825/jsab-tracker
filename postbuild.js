9c3fb299054a5ee2f9636225c11d4f6a9eaa7edf
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-11-22 17:50:16 +0000

    MORE minification

--- postbuild.js
+++ postbuild.js
@@ -6,39 +6,53 @@ export default undefined;
 // Code that runs after the build is complete.
 
 // this further minifies the main js code
-const jsfilename = fs.readdirSync("dist/assets").filter((filename) => filename.endsWith(".js"))[0];
-const code = fs.readFileSync(`dist/assets/${jsfilename}`, "utf8");
+const mainjs = fs.readdirSync("dist/assets").filter((filename) => filename.endsWith(".js"))[0];
+const partytown = fs.readdirSync("dist/~partytown").filter((filename) => filename.endsWith(".js"));
+const partytowndebug = fs.readdirSync("dist/~partytown/debug").filter((filename) => filename.endsWith(".js"));
 
-const query = querystring.stringify({
-	input: code,
+minify(mainjs);
+
+partytown.forEach((file) => {
+	minify(file);
 });
 
-const req = https.request(
-	{
-		method: "POST",
-		hostname: "www.toptal.com",
-		path: "/developers/javascript-minifier/api/raw",
-	},
-	function (res) {
-		// if the statusCode isn't what we expect, get out of here
-		if (res.statusCode !== 200) {
-			console.log("StatusCode=" + res.statusCode);
-			return;
+partytowndebug.forEach((debugfile) => {
+	minify(debugfile);
+});
+
+function minify(jsfilename) {
+	const code = fs.readFileSync(`dist/${jsfilename}`, "utf8");
+
+	const query = querystring.stringify({
+		input: code,
+	});
+
+	const req = https.request(
+		{
+			method: "POST",
+			hostname: "www.toptal.com",
+			path: "/developers/javascript-minifier/api/raw",
+		},
+		function (res) {
+			// if the statusCode isn't what we expect, get out of here
+			if (res.statusCode !== 200) {
+				console.log("StatusCode=" + res.statusCode);
+				return;
+			}
+
+			let body = "";
+			res.on("data", function (chunk) {
+				body += chunk;
+			});
+			res.on("end", function () {
+				fs.writeFileSync(`dist/${jsfilename}`, body);
+			});
 		}
-
-		let body = "";
-		res.on("data", function (chunk) {
-			body += chunk;
-		});
-		res.on("end", function () {
-			fs.writeFileSync(`dist/assets/${jsfilename}`, body);
-		});
-
-	}
-);
-req.on("error", (e) => {
-	throw e;
-});
-req.setHeader("Content-Type", "application/x-www-form-urlencoded");
-req.setHeader("Content-Length", query.length);
-req.end(query, "utf8");
+	);
+	req.on("error", (e) => {
+		throw e;
+	});
+	req.setHeader("Content-Type", "application/x-www-form-urlencoded");
+	req.setHeader("Content-Length", query.length);
+	req.end(query, "utf8");
+}
