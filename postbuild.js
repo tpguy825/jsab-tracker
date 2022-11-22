ab2ce4cb0a16c75ce50ef24221078e56bc93338a
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-11-22 17:09:01 +0000

    jquery

--- postbuild.js
+++ postbuild.js
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import $ from "jquery";
 export default undefined;
 
 // Code that runs after the build is complete.
@@ -6,9 +7,8 @@ export default undefined;
 const jsfilename = fs.readdirSync("dist/assets").filter((filename) => filename.endsWith(".js"))[0];
 const code = fs.readFileSync(`dist/assets/${jsfilename}`, "utf8");
 
-fetch("https://www.toptal.com/developers/javascript-minifier/api/raw", { method: "post", body: code })
-	.then((response) => response.text())
-	.then((minified) => {
+$.post("https://www.toptal.com/developers/javascript-minifier/api/raw", code)
+	.done((minified) => {
 		if (minified.startsWith("{")) {
 			console.error("Error minifying code:", minified);
 			return;
