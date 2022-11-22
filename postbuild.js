bdb87cea08fac30cb4f387aa828649f93fe52975
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-11-22 17:27:26 +0000

    use https.request instead

--- postbuild.js
+++ postbuild.js
@@ -1,5 +1,6 @@
 import * as fs from "fs";
-import $ from "jquery";
+import https from "https";
+import querystring from "querystring";
 export default undefined;
 
 // Code that runs after the build is complete.
@@ -7,11 +8,29 @@ export default undefined;
 const jsfilename = fs.readdirSync("dist/assets").filter((filename) => filename.endsWith(".js"))[0];
 const code = fs.readFileSync(`dist/assets/${jsfilename}`, "utf8");
 
-$.post("https://www.toptal.com/developers/javascript-minifier/api/raw", code)
-	.done((minified) => {
-		if (minified.startsWith("{")) {
-			console.error("Error minifying code:", minified);
+const query = querystring.stringify({
+	input: code,
+});
+
+const req = https.request(
+	{
+		method: "POST",
+		hostname: "www.toptal.com",
+		path: "/developers/javascript-minifier/api/raw",
+	},
+	function (res) {
+		// if the statusCode isn't what we expect, get out of here
+		if (res.statusCode !== 200) {
+			console.log("StatusCode=" + res.statusCode);
 			return;
 		}
-		fs.writeFileSync(`dist/assets/${jsfilename}`, minified);
-	});
+
+		fs.writeFileSync(`dist/assets/${jsfilename}`, res.read());
+	}
+);
+req.on("error", (e) => {
+	throw e;
+});
+req.setHeader("Content-Type", "application/x-www-form-urlencoded");
+req.setHeader("Content-Length", query.length);
+req.end(query, "utf8");
