010a0aa60feb5e0ae9e500e6e6cd4d4de5c1d314
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2023-01-02 20:17:53 +0000

    Update cloudflare-worker.js

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -2,7 +2,10 @@ addEventListener("fetch", (event) => {
 	event.respondWith(handleRequest(event));
 });
 
-const jsabassets = `http://jsab.tpguy825.cf/assets`;
+const jsabassets = "https://jsab-tracker.vercel.app";
+const otherfiles = "https://raw.githubusercontent.com/tpguy825/other/master/public/cdn";
+const count = () => fetch("https://api.countapi.xyz/hit/cdn.tpguy825.cf-files", { method: "GET" }).then((r) => r.json())
+// NOTE: to get the actual count, divide it by 2. If it's odd, -1
 
 /**
  * @param {Event} event
@@ -11,6 +14,7 @@ const jsabassets = `http://jsab.tpguy825.cf/assets`;
  */
 async function serveAsset(event) {
 	const url = new URL(event.request.url);
+	if(url.pathname.startsWith("/files/Onett")) console.warn((await count()).value)
 	const cache = caches.default;
 	let response = await cache.match(event.request);
 	if (!response) {
@@ -34,6 +38,8 @@ async function serveAsset(event) {
 function getUrl(pathname) {
 	if (pathname.startsWith("/jsab")) {
 		return `${jsabassets}${pathname.slice(5)}`;
+	} else if(pathname.startsWith("/files")) {
+		return `${otherfiles}${pathname.slice(6)}`
 	} else {
 		return new Response("Invalid path", { status: 404 });
 	}
